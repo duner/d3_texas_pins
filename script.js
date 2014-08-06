c3ebf5e124bc782b8edbb3ff759c84ab241132bc
duner <asduner@gmail.com>
2014-08-06 17:52:11 -0500

    Colors!

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 
 		var projection = d3.geo.mercator()
 			.center(d3.geo.centroid(texas))
-		    .scale(width)
+		    .scale(width * 2)
 		    .translate([width / 2, height / 2]);
 
 		var path = d3.geo.path()
@@ -32,8 +32,7 @@ $(document).ready(function(){
 			svg.append("path")
 				.datum(texas)
 				.attr("d", path)
-				.style('fill', 'black')
-				.style('stroke', 'black');
+				.style('fill', '#4aa1cc')
 
 		    svg.selectAll("circle")
 				.data([point])
@@ -42,15 +41,16 @@ $(document).ready(function(){
 					.attr("cx", function (d) { return projection(d.coords)[0]; })
 					.attr("cy", function (d) { return projection(d.coords)[1]; })
 					.attr("r", function(d) { return (width / mapRatio) * .01 + 'px'; })
-					.attr("fill", "red");
+					.attr("fill", "#9772c5");
 
-			svg.append('text')
+			svg.selectAll('text')
 				.data([point])
 				.enter()
 				.append("text")
-					.attr('x', function(d){ return console.log(d); path.centroid(d.coords)[0];})
-					.attr('y', function(d){ return path.centroid(d.coords)[1];})
-					.attr('text',function(d){return d.name;})
+					.attr('x', function(d){ return projection(d.coords)[0]; })
+					.attr('y', function(d){ return projection(d.coords)[1]; })
+					.attr('text',function(d){ console.log(d); return d.name;})
+					.attr('fill', 'black')
 					.style("font-size","14px");
 
 		}
