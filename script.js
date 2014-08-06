ec7d47f6c46720369d54ea8088bddf34d19fa5f7
duner <asduner@gmail.com>
2014-08-05 20:12:01 -0500

    add topojson

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-d3.json('texas.geojson', function(texas) {
+d3.json('texas.topo.json', function(texas) {
+	
 	console.log(texas);
+
+	var w = 500;
+	var h = 500;
+
+	var svg = d3.select("body").append("svg")
+		.attr("width", w)
+		.attr("height", h);
+
+	svg.append("path")
+		.datum(topojson.feature(texas, texas.objects.subunits))
+		.attr("d", d3.geo.path().projection(d3.geo.mercator()));
+
+
 });
