3c206b06172981839a65c6af616a5795720f0723
duner <asduner@gmail.com>
2014-08-06 11:14:30 -0500

    I made Texas

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-d3.json('texas.topo.json', function(texas) {
-	
-	console.log(texas);
+d3.json('texas.topo.json', function(data) {
 
 	var w = 500;
 	var h = 500;
+
+	var projection = d3.geo.albersUsa()
+	    .scale(1000)
+	    .translate([w / 2, h / 2]); 
+
+	var path = d3.geo.path()
+	    .projection(projection);
 
 	var svg = d3.select("body").append("svg")
 		.attr("width", w)
 		.attr("height", h);
 
-	svg.append("path")
-		.datum(topojson.feature(texas, texas.objects.subunits))
-		.attr("d", d3.geo.path().projection(d3.geo.mercator()));
+	feature = topojson.feature(data, data.objects.texas);
+	console.log(feature);
 
+	svg.append("path")
+		.datum(feature)
+		.attr("d", path)
+		.style('fill', 'black')
+		.style('stroke', 'black');
 
 });
